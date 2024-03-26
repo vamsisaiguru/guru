995755a134528566612bf464c177e93343260ef4
vamsisaiguru <gurusaivamsi@gmail.com>
2024-03-26 10:41:19 -0400

    give the commite

---
+++ src/selenium2/Logical1.java
@@ -0,0 +1,22 @@
+package selenium2;
+
+public class Logical1 {
+
+	
+		  public static void main(String[] args) {
+
+		    // && operator
+		    System.out.println((5 > 3) && (8 > 5));  
+		    System.out.println((5 > 3) && (8 < 5));  
+
+		    // || operator
+		    System.out.println((5 < 3) || (8 > 5));  
+		    System.out.println(5 > 3);  // true
+		    System.out.println((5 < 3) || (8 < 5));  
+
+		    // ! operator
+		    System.out.println(!(5 == 3));  
+		    System.out.println(!(5 > 3));  
+		  }
+		}
+
